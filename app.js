c4cee6a70a8365bc910d13b4b4a905fd3295cc66
songsamuel <songsme2886@gmail.com>
2024-11-09 00:11:45 +0900

    백엔드 코드 수정 완료

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import connect from './schemas/index.js';
-import todosRouter from './routers/todos.router.js';
-import errorHandlerMiddleware from './middlewares/error-handler.middleware.js';
+import express from "express";
+import connect from "./schemas/index.js";
+import todosRouter from "./routers/todos.router.js";
+import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
 
 const app = express();
 const PORT = 3000;
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 미들웨어 3
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 // 미들웨어 4
 app.use((req, res, next) => {
-  console.log('Request URL:', req.originalUrl, ' - ', new Date());
+  console.log("Request URL:", req.originalUrl, " - ", new Date());
   next();
 });
 
@@ -27,15 +27,15 @@ connect();
 const router = express.Router();
 
 // 미들웨어  5
-app.use('/api', [router, todosRouter]);
+app.use("/api", [router, todosRouter]);
 
-router.get('/', (req, res) => {
-  return res.json({ message: '연습용 사이트입니다.' });
+router.get("/", (req, res) => {
+  return res.json({ message: "연습용 사이트입니다." });
 });
 
 // 에러 처리 미들웨어를 등록한다.
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
-  console.log('네가 원하는 ' + PORT + '로 열렸습니다. 확인해보세요.');
+  console.log("네가 원하는 " + PORT + "로 열렸습니다. 확인해보세요.");
 });
